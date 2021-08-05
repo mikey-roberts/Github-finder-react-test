beb1e32719bf05b5c3d47d434357238cd9a2332f
mikey-roberts <mikey_roberts@outlook.com>
2021-08-05 13:38:13 +0100

    replacing with functional components

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Users from './components/users/Users';
+import './App.css';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='App'>
+      <Navbar/>
+      <div className="container">
+      <Users/>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
